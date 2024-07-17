fcf107941e11db52eb791bb4b8cea0340f8ca10f
satriyo <ryorizkyansah@gmail.com>
2024-07-17 17:37:33 +0700

    static and constant

---
+++ #11/static.php
@@ -0,0 +1,41 @@
+<?php 
+
+    // class ContohStatic{
+
+    //     public static $angka = 1;
+
+    //     public static function halo(){
+    //         return "Halo " . self::$angka+2 . " kali.";
+    //     }
+
+    // }
+
+    // echo ContohStatic::$angka;
+    // echo "<br>";
+    // echo ContohStatic::halo();
+    // echo "<hr>";
+    // echo ContohStatic::halo();
+
+    class Contoh {
+        public static $angka = 1;
+
+        public static function halo(){
+            return "Halo " . self::$angka++ . " kali. <br>";
+        }
+    }
+
+    $obj = new Contoh;
+
+    echo $obj->halo();
+    echo $obj->halo();
+    echo $obj->halo();
+
+    echo "<hr>";
+    
+    echo $obj->halo();
+    echo $obj->halo();
+    echo $obj->halo();
+
+
+
+?>
---
+++ #12/constant.php
@@ -0,0 +1,19 @@
+<?php 
+
+    // define('NAMA', 'Satriyo Rizkyansah');
+    // const UMUR = 20;
+
+    // echo NAMA;
+    // echo "<br>";
+    // echo UMUR;
+
+    class Coba{
+        // define('NAMA', 'Satriyo Rizkyansah');
+
+        const NAMA = 'Satriyo Rizkyansah';
+    }
+
+    echo Coba::NAMA;
+    echo "<br>";
+    echo __LINE__;
+?>
